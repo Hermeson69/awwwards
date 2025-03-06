d348d13461dcc9377be405fcf7ac5e9e5dc8f483
Hermeson <hermesonalves256@gmail.com>
2025-03-06 12:20:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <main>
+        <h1 className="text-3xl font-bold font-zentry">
+          Hello world!
+        </h1>
+      </main>
+    </>
+  )
+}
+
+export default App
