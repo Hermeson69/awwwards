f19c201c1a4bba61c9b915f46f3279f85d6111b1
Hermeson <hermesonalves256@gmail.com>
2025-03-07 16:46:32 -0300

    feat: iniciando about section

---
+++ src/components/About.tsx
@@ -0,0 +1,54 @@
+import gsap from 'gsap'
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
+
+const About = () =>{
+    useGSAP(()=>{
+        const clipAnimation = gsap.timeline(
+            {
+                scrollTrigger:{
+                    trigger: '#clip',
+                    start: 'center center',
+                    end: '+=800 center',
+                    scrub: 0.5,
+                    pin: true,
+                    pinSpacing: true,
+                }
+            }
+        )
+        clipAnimation.to('.mask-clip-path',{
+            width: '100vw',
+            height: '100vh',
+            borderRadius: 0
+        })
+    })
+    return (
+        <div id = "about" className="min-h-screen w-screen">
+            <div className="relative mb-8 mt-36 flex flex-col">
+                <h2 className="font-general text-center text-sm uppercase md: text-[10px]">
+                    Welcone to Zentry
+                </h2>
+                <div className="mt-5 text-center text-sl uppercase leading-0.8 md: text-[6rem]">
+                    Disc<b>o</b>ver the world´s <br /> l<b>a</b>rgest shered asventure
+                </div>
+
+                <div className="about-subtext">
+                    <p>The Game fo Game begins-your life now an eoic MMORPG</p>
+                    <p>Zentry unites every player from countless game and platforms</p>
+                </div>
+            </div>
+            <div className="h-dvh w-screen "id="clip">
+                <div className="mask-clip-path about-image">
+                    <img
+                    src="img/about.webp"
+                    alt= "Backgroud"
+                    className="absolut left-0 top-0 size-full object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
